5c34bfa5e43364c1dd6d4c2192525aa89beecfca
John Moore <john@ncub8.com>
2015-02-23 18:57:31 +0000

    added a couple of tests

--- tests/test_views_acceptance.py
+++ tests/test_views_acceptance.py
@@ -56,6 +56,31 @@ class TestViews(unittest.TestCase):
         button = self.browser.find_by_css("button[type=submit]")
         button.click()
         self.assertEqual(self.browser.url, "http://0.0.0.0:5000/login")
+        
+    def testAddPostLoggedIn(self):
+        self.browser.visit("http://0.0.0.0:5000/login")
+        self.browser.fill("email", "alice@example.com")
+        self.browser.fill("password", "test")
+        button = self.browser.find_by_css("button[type=submit]")
+        button.click()
+        self.assertEqual(self.browser.url, "http://0.0.0.0:5000/")
+        #self.browser.click_link_by_text('Log Out')
+        self.browser.visit('http://0.0.0.0:5000/post/add')
+        self.assertEqual(self.browser.url, "http://0.0.0.0:5000/post/add")
+        self.browser.fill("title", "Testing Testing")
+        self.browser.fill("content", "Here is some content")
+        button = self.browser.find_by_css("button[type=submit]")
+        button.click()
+        self.assertEqual(self.browser.url, "http://0.0.0.0:5000/")
+        
+    def testAddPostNotLoggedIn(self):
+        self.browser.visit("http://0.0.0.0:5000/login")
+        self.browser.fill("email", "bob@example.com")
+        self.browser.fill("password", "test")
+        button = self.browser.find_by_css("button[type=submit]")
+        button.click()
+        self.browser.visit('http://0.0.0.0:5000/post/add')
+        self.assertEqual(self.browser.url, "http://0.0.0.0:5000/login?next=%2Fpost%2Fadd")
 
 if __name__ == "__main__":
     unittest.main()
